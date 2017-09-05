416fc0b9b36ec50b6ca480d46bcdf8ba5f61f1c1
Pim de Wit <pim@pdw.io>
2017-09-05 21:55:49 +0100

    feat(pwa): pdw can not into PWA

--- src/app.js
+++ src/app.js
@@ -1,23 +1,18 @@
 window.addEventListener('load', () => {
-  requestAnimationFrame(() => {
-    requestAnimationFrame(() => {
-      let polyfillScript = document.createElement('script');
-      polyfillScript.src =
-          'bower_components/webcomponentsjs/webcomponents-loader.js';
-      polyfillScript.async = true;
+  const polyfillScript = document.createElement('script');
+  polyfillScript.src = 'bower_components/webcomponentsjs/webcomponents-loader.js';
+  polyfillScript.async = true;
 
-      document.head.appendChild(polyfillScript);
+  document.head.appendChild(polyfillScript);
 
-      const font = document.createElement('link');
-      font.rel = 'stylesheet';
-      font.href = 'https://fonts.googleapis.com/css?family=Work+Sans:400';
+  const font = document.createElement('link');
+  font.rel = 'stylesheet';
+  font.href = 'https://fonts.googleapis.com/css?family=Work+Sans:400';
 
-      const pdwApp = document.createElement('link');
-      pdwApp.rel = 'import';
-      pdwApp.href = 'src/pdw-app/pdw-app.html';
+  const pdwApp = document.createElement('link');
+  pdwApp.rel = 'import';
+  pdwApp.href = 'src/pdw-app/pdw-app.html';
 
-      document.head.appendChild(pdwApp);
-      document.head.appendChild(font);
-    });
-  });
+  document.head.appendChild(pdwApp);
+  document.head.appendChild(font);
 });
