6e98d3c77330a5fb20340f8767628edd95d8befc
Pim de Wit <pim@pdw.io>
2017-11-18 14:06:09 +0000

    feat(shell): styles refactor

--- src/pdw-app/pdw-app.js
+++ src/pdw-app/pdw-app.js
@@ -1,87 +1,83 @@
 class PdwApp extends Polymer.Element {
 
-  static get is() { return 'pdw-app'; }
+  static get is() {
+    return 'pdw-app';
+  }
 
   static get properties() {
-                            return {
-                            page: {
-                            type: String,
-                            reflectToAttribute: true,
-                            observer: '_pageChanged',
-                            },
-                            rootPattern: String,
-                            routeData: Object,
-                            subroute: String,
-                            };
-                            }
+    return {
+      page: {
+        type: String,
+        reflectToAttribute: true,
+        observer: '_pageChanged',
+      },
+      rootPattern: String,
+      routeData: Object,
+      subroute: String,
+    };
+  }
 
   static get observers() {
-                           return [
-                           '_routePageChanged(routeData.page)',
-                           ];
-                           }
+    return [
+      '_routePageChanged(routeData.page)',
+    ];
+  }
 
   constructor() {
-                  super();
+    super();
 
-                  Polymer.RenderStatus.afterNextRender(this, () => {
-                  // Get root pattern for app-route, for more info about `rootPath` see:
-                  // https://www.polymer-project.org/2.0/docs/upgrade#urls-in-templates
-                  this.rootPattern = (new URL(this.rootPath)).pathname;
+    Polymer.RenderStatus.afterNextRender(this, () => {
+      this.rootPattern = (new URL(this.rootPath)).pathname;
 
-                  this.setAttribute('active', '');
-                  });
-                  }
+      this.setAttribute('active', '');
+    });
+  }
 
   connectedCallback() {
-                        super.connectedCallback();
-                        }
+    super.connectedCallback();
+  }
 
   /**
-    * Accessibility: Focus the main content, to skip the main navigation.
-    * @private
-    */
+   * Accessibility: Focus the main content, to skip the main navigation.
+   * @private
+   */
   _focusMainContent() {
     this.$.maincontent.focus();
   }
 
   /**
-    *
-    * @param {String} page
-    * @private
-    */
+   *
+   * @param {String} page
+   * @private
+   */
   _routePageChanged(page) {
-                            // Polymer 2.0 will call with `undefined` on initialization.
-                            // Ignore until we are properly called with a string.
-                            if (page === undefined) {
-                            return;
-                            }
+    if (page === undefined) {
+      return;
+    }
 
-                            // If no page was found in the route data, page will be an empty string.
-                            // Default to 'home' in that case.
-                            this.page = page || 'home';
-                            }
+    this.page = page || 'home';
+  }
 
   /**
-    * Request the page or go to the 404.
-    * @param {String} page
-    * @private
-    */
+   * Request the page or go to the 404.
+   * @param {String} page
+   * @private
+   */
   _pageChanged(page) {
-                       // Load page import on demand. Show 404 page if fails
-                       if (page === 'index.html') return;
+    // Load page import on demand. Show 404 page if fails
+    if (page === 'index.html') return;
 
-                       const resolvedPageUrl = this.resolveUrl('../pages/pdw-' + page + '.html');
-                       Polymer.importHref(
-                       resolvedPageUrl,
-                       null,
-                       this._showPage404.bind(this),
-                       true);
-                       }
+    const resolvedPageUrl = this.resolveUrl('../pages/pdw-' + page + '.html');
+    Polymer.importHref(
+        resolvedPageUrl,
+        null,
+        this._showPage404.bind(this),
+        true);
+  }
 
   _showPage404() {
-  this.page = '404';
-}
+    this.page = '404';
+  }
 }
 
 window.customElements.define(PdwApp.is, PdwApp);
